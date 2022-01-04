467486ff1f1f2aab961ef55b8642cb7c2a1d72e9
Oskar <oskar.schilling@zhr.pl>
2022-01-04 17:45:04 +0100

    1.4 Extract Constants

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,13 +2,17 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-	private int[] numbers = new int[12];
+	public static final int NUMBERS_MAX_SIZE = 12;
+	public static final int IS_EMPTY = -1;
+	public static final int IS_FULL = 11;
+
+	private int[] numbers = new int[NUMBERS_MAX_SIZE];
 
 	public int getTotal() {
 		return total;
 	}
 
-	private int total = -1;
+	private int total = IS_EMPTY;
 
 	public void countIn(int in) {
 		if (!isFull())
@@ -16,22 +20,22 @@ public class DefaultCountingOutRhymer {
 	}
 
 	public boolean callCheck() {
-		return total == -1;
+		return total == IS_EMPTY;
 	}
 
 	public boolean isFull() {
-		return total == 11;
+		return total == IS_FULL;
 	}
 
 	protected int peekaboo() {
 		if (callCheck())
-			return -1;
+			return IS_EMPTY;
 		return numbers[total];
 	}
 
 	public int countOut() {
 		if (callCheck())
-			return -1;
+			return IS_EMPTY;
 		return numbers[total--];
 	}
 
--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -2,6 +2,7 @@ package edu.kis.vh.nursery.list;
 
 public class IntLinkedList {
 
+	public static final int IS_EMPTY = -1;
 	private Node last;
 	private int i;
 
@@ -25,13 +26,13 @@ public class IntLinkedList {
 
 	public int top() {
 		if (isEmpty())
-			return -1;
+			return IS_EMPTY;
 		return last.getValue();
 	}
 
 	public int pop() {
 		if (isEmpty())
-			return -1;
+			return IS_EMPTY;
 		int ret = last.getValue();
 		last = last.getPrev();
 		return ret;
