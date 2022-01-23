de8ef8b6826fecf27a5896918d2dc47aa377faf3
jchrustowski <bolek9lodz99@gmail.com>
2022-01-23 23:38:15 +0100

    2.1.2 Rewritten DefaultCountingOutRhymer class (using IntArrayStack)

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -5,65 +5,38 @@ package edu.kis.vh.nursery;
  */
 public class DefaultCountingOutRhymer {
 
-	public static final int NUMBERS_MAX_SIZE = 12;
-	public static final int IF_EMPTY = -1;
-	public static final int INITIAL_RHYMER_INDEX = -1;
+	private final IntArrayStack stack;
 
-	private final int[] numbers = new int[NUMBERS_MAX_SIZE];
+	public DefaultCountingOutRhymer() {
+		this.stack = new IntArrayStack();
+	}
 
-	/**
-	 * This method returns the total count of numbers stored.
-	 * @return current size of Rhymer.
-	 */
+	public DefaultCountingOutRhymer(IntArrayStack stack) {
+		this.stack = stack;
+	}
+
 	public int getTotal() {
-		return total;
+		return stack.getTotal();
 	}
 
-	private int total = INITIAL_RHYMER_INDEX;
-
-	/**
-	 * This method appends a new number to Rhymer if Rhymer is not full, otherwise nothing happens.
-	 * @param in the value to set to the next element
-	 */
 	public void countIn(int in) {
-		if (!isFull())
-			numbers[++total] = in;
+		stack.countIn(in);
 	}
 
-	/**
-	 * This method checks if it is possible to peek or pop a number from Rhymer.
-	 * @return {@code true} if Rhymer is in initial state, otherwise {@code false}.
-	 */
 	public boolean callCheck() {
-		return total == INITIAL_RHYMER_INDEX;
+		return stack.callCheck();
 	}
 
-	/**
-	 * This method checks if Rhymer is full
-	 * @return {@code true} if Rhymer is full otherwise {@code false}.
-	 */
 	public boolean isFull() {
-		return total == NUMBERS_MAX_SIZE-1;
+		return stack.isFull();
 	}
 
-	/**
-	 * This method peeks an element on the current position of Rhymer.
-	 * @return {@code IF_EMPTY} if Rhymer is empty otherwise the value of current element.
-	 */
-	protected int peekaboo() {
-		if (callCheck())
-			return IF_EMPTY;
-		return numbers[total];
+	public int peekaboo() {
+		return stack.peekaboo();
 	}
 
-	/**
-	 * This method pops the current item of Rhymer if Rhymer has some elements, otherwise it returns {@code IF_EMPTY}
-	 * @return the value of removed element if exists.
-	 */
 	public int countOut() {
-		if (callCheck())
-			return IF_EMPTY;
-		return numbers[total--];
+		return stack.countOut();
 	}
 
 }
